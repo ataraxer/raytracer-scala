1da7f5007fbb149b01cd9d34272cf226cfd614fb
Anton Karamanov <ataraxer@gmail.com>
2014-11-30 23:19:47 +0300

    FilmSaver: use byte buffer to construct bmp headers

--- src/main/scala/FilmSaver.scala
+++ src/main/scala/FilmSaver.scala
@@ -1,6 +1,7 @@
 package com.ataraxer.apps.raytracer.scala
 
 import java.io.FileOutputStream
+import java.nio.{ByteBuffer, ByteOrder}
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,29 +34,39 @@ object FilmSaver {
       (for (i <- 0 to 3)
       yield (value >> 8 * i).toByte).toList
 
-    val bmpFileHeader: List[Byte] =
-      List[Byte]('B'.toByte, 'M'.toByte) ++ // biType 2
-        toByte(fileSize) ++                 // biSize 4
-        List[Byte](0,0,0,0) ++              // biReserved{1,2} 4
-        List[Byte](54,0,0,0)                // biOffBits 4
+    val bmpFileHeader = ByteBuffer.allocate(14)
+    bmpFileHeader.order(ByteOrder.LITTLE_ENDIAN)
 
-    val bmpInfoHeader: List[Byte] =
-      List[Byte](40, 0, 0, 0) ++            // biSize 4
-      toByte(Raytracer.width) ++            // biWidth 4
-      toByte(Raytracer.height) ++           // biHeight 4
-      List[Byte](1,0) ++                    // biPlanes 2
-      List[Byte](24,0) ++                   // biBitCount 2
-      List[Byte](0,0,0,0) ++                // biCompression 4
-      toByte(imageSize) ++                  // biSizeImage 4
-      toByte(ppm) ++                        // biXPelsPerMeter 4
-      toByte(ppm) ++                        // biYPelsPerMeter 4
-      List[Byte](0,0,0,0) ++                // biClrUsed 4
-      List[Byte](0,0,0,0)                   // biClrImportant 4
+    bmpFileHeader.put('B'.toByte)                // biType 2
+    bmpFileHeader.put('M'.toByte)                // --------
+    bmpFileHeader.putInt(fileSize)               // biSize 4
+    bmpFileHeader.put(Array[Byte](0, 0, 0, 0))   // biReserved{1,2} 4
+    bmpFileHeader.put(Array[Byte](54, 0, 0, 0))  // biOffBits 4
+
+    bmpFileHeader.rewind()
+
+
+    val bmpInfoHeader = ByteBuffer.allocate(40)
+    bmpInfoHeader.order(ByteOrder.LITTLE_ENDIAN)
+    bmpInfoHeader.put(Array[Byte](40, 0, 0, 0))  // biSize 4
+    bmpInfoHeader.putInt(Raytracer.width)        // biWidth 4
+    bmpInfoHeader.putInt(Raytracer.height)       // biHeight 4
+    bmpInfoHeader.put(Array[Byte](1, 0))         // biPlanes 2
+    bmpInfoHeader.put(Array[Byte](24,0))         // biBitCount 2
+    bmpInfoHeader.put(Array[Byte](0, 0, 0, 0))   // biCompression 4
+    bmpInfoHeader.putInt(imageSize)              // biSizeImage 4
+    bmpInfoHeader.putInt(ppm)                    // biXPelsPerMeter 4
+    bmpInfoHeader.putInt(ppm)                    // biYPelsPerMeter 4
+    bmpInfoHeader.put(Array[Byte](0, 0, 0, 0))   // biClrUsed 4
+    bmpInfoHeader.put(Array[Byte](0, 0, 0, 0))   // biClrImportant 4
+
+    bmpInfoHeader.rewind()
+
 
     val file: FileOutputStream = new FileOutputStream(filmDirectory + filmName)
     try {
-      file.write(bmpFileHeader.toArray)
-      file.write(bmpInfoHeader.toArray)
+      file.write(bmpFileHeader.array)
+      file.write(bmpInfoHeader.array)
       for (pixel <- pixels) {
         file.write(pixel.toArray)
       }
